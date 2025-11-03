a95121c4efd6df81b60c55a4b303719cdad7e3a0
Joseph Balog <balohyosyp.96@gmail.com>
2025-11-03 16:40:22 +0200

    Modified settings.py

--- DOEJoseph/settings.py
+++ DOEJoseph/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL hozzáadása (ez hiányzott)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS - csak akkor add hozzá, ha a könyvtár létezik
